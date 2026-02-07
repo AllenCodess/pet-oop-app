0eec9c3956295c6fc3fdeb7ab76099a26eebec0e
AllenCodess <Aph296@protonmail.com>
2026-02-06 21:55:52 -0500

    created object and added functions in them. called other values inside the object

---
+++ app.js
@@ -0,0 +1,23 @@
+// create the pet object
+
+const pet = {
+  name: "Biscuit",
+  hunger: 5,
+  happiness: 5,
+
+  feed: function () {
+    if (this.hunger > 0) {
+      this.hunger--;
+    }
+  },
+
+  play: function () {
+    if (this.happiness < 10) {
+      this.happiness++;
+    }
+  },
+
+  getStatus: function () {
+    return (this.name + " 's " + this.hunger + "| Happiness: " + this, this.happiness);
+  },
+};
